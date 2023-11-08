d301b9794731eaa1af4f509edf4bf1293d666140
UpasanaRaghav <raghavupasana44@gmail.com>
2023-11-09 00:03:52 +0530

    Initialisation

---
+++ WanderMate/src/components/Map/Map.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+const Map = ()=>{
+  return (
+    <div>
+      <h1>Map</h1>
+      </div>
+    )
+}
+
+  
+  
+     
+      
+export default Map
---
+++ WanderMate/src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+const PlaceDetails = ()=>{
+  return (
+    <div>
+      <h1>Place Details</h1>
+      </div>
+    )
+}
+
+  
+  
+     
+      
+export default PlaceDetails
