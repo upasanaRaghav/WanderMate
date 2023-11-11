6f95a7d2cc448787bb855fb24a82cb3ed9302295
UPASANA RAGHAV <98830941+upasanaRaghav@users.noreply.github.com>
2023-11-11 12:27:21 +0530

    Add files via upload

---
+++ src/components/List/List.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+const List = ()=>{
+  return (
+    <div>
+      <h1>Card detail, List</h1>
+      </div>
+    )
+}
+
+  
+  
+     
+      
+export default List
